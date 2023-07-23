1c9f47d1b0fbfefddc1e1d34193d32a70abff7b4
von-moyo <florishadekogbe@gmail.com>
2023-07-23 21:28:19 +0100

    changed images path

--- src/components/InnerPages/ProjectDetails/Content.jsx
+++ src/components/InnerPages/ProjectDetails/Content.jsx
@@ -11,7 +11,7 @@ function Content() {
   return (
       <div className="container portfolioo">
         <img
-          src="/assets/imgs/portfolio/Hero-X_large.jpg"
+          src={`/assets/imgs/portfolio/Hero-X_large.jpg`}
           alt=""
           className="heroImage"
         />
@@ -25,12 +25,12 @@ function Content() {
         <div className="firstImageSection">
           <img
             alt=""
-            src="/assets/imgs/portfolio/Delivery man-X_large.jpg"
+            src={`/assets/imgs/portfolio/Delivery man-X_large.jpg`}
           />
           <iframe className="iframe" src="https://player.vimeo.com/video/846596223?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;background=1&amp;loop=1" width="590" height="664" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="Yoowa Display Animation 4k"></iframe>
           <img
             alt=""
-            src="/assets/imgs/portfolio/Billboard-X_large.jpg"
+            src={`/assets/imgs/portfolio/Billboard-X_large.jpg`}
           />
         </div>
         <div className="secondImageSection">
@@ -48,14 +48,14 @@ function Content() {
             <img
             className="stickers"
             alt=""
-            src="/assets/imgs/portfolio/Stickers-X_large.jpg"
+            src={`/assets/imgs/portfolio/Stickers-X_large.jpg`}
             />
             {/* <div className="smile">
               <iframe className="iframe" src="https://player.vimeo.com/video/846596077?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;background=1&amp;loop=1" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="Smile Word Language 4k"></iframe>
             </div> */}
             <img
             alt=""
-            src="/assets/imgs/portfolio/Shirts-X_large.jpg"
+            src={`/assets/imgs/portfolio/Shirts-X_large.jpg`}
             />
           </div>
         </div>
@@ -64,38 +64,38 @@ function Content() {
           <img
             className="people"
             alt=""
-            src="/assets/imgs/portfolio/People-X_large.jpg"
+            src={`/assets/imgs/portfolio/People-X_large.jpg`}
           />
           <img
             alt=""
-            src="/assets/imgs/portfolio/Food pack-X_large.jpg"
+            src={`/assets/imgs/portfolio/Food pack-X_large.jpg`}
           />
           <img
-            className="banner"
-            alt=""
-            src="/assets/imgs/portfolio/Large Banner-X_large.png"
+          className="banner"
+          alt=""
+          src={`/assets/imgs/portfolio/Large Banner-X_large.png`}
           />
         </div>
         <div className="fourthImageSection">
           <img
             alt=""
-            src="/assets/imgs/portfolio/Hand holding phone-X_large.jpg"
+            src={`/assets/imgs/portfolio/Hand holding phone-X_large.jpg`}
           />
           <img
             alt=""
-            src="/assets/imgs/portfolio/Woman eating-X_large.jpg"
+            src={`/assets/imgs/portfolio/Woman eating-X_large.jpg`}
           />
         </div>
         <div className="firstImageSection">
           <iframe className="iframe" src="https://player.vimeo.com/video/846610612?title=0&amp;byline=0&amp;portrait=0&amp;speed=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;background=1&amp;loop=1" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="Food moving with stop motion effect 4k"></iframe>
           <img
             alt=""
-            src="/assets/imgs/portfolio/Bottle floating-X_large.jpg"
+            src={`/assets/imgs/portfolio/Bottle floating-X_large.jpg`}
           />
         </div>
         <img
             alt=""
-            src="/assets/imgs/portfolio/Shirts-X_large.jpg"
+            src={`/assets/imgs/portfolio/Shirts-X_large.jpg`}
         />
       </div>
   );
